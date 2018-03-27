664e083ee7030058ebb71091d38ae79cec21af9b
lamasnino <amelissalamas@gmail.com>
2018-03-27 00:50:03 -0400

    final 002

---
+++ assignments/001/lamasnino/assignment/my_app/meet3_firstattempt_crimtech.py
@@ -0,0 +1,31 @@
+# save as .html
+
+<!DOCTYPE HTML>
+>html>
+    <head>
+        <title><Games</title>
+        <link rel = "stylesheet"
+        text="text/css
+        "href ="
+        hello.css">
+    </head>
+<body>
+    <p> The Crimson.</p>
+    <p>
+    
+    <p>Fun.</p>
+    <ul>
+        <li>Fun.</li
+        >
+        <li> They are fun games.</
+        li>
+    </ul>
+
+    <ul>
+    <li> around each element of list
+    close with </ul>
+</body>
+    
+</html>
+
+
